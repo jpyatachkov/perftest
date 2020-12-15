f05c32eb3bfea41539d035d81c18e7fdde222e51
jpyatachkov <cvkucherov@yandex.ru>
2020-12-16 00:43:16 +0300

    Add actuator module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import {ActuatorModule} from '@glosur/nestjs-actuator';
 import {CartController} from './cart.controller';
 import {CartService} from './cart.service';
 import {Module} from '@nestjs/common';
@@ -5,13 +6,15 @@ import {Module} from '@nestjs/common';
 const imports = [];
 
 if (process.env.ADMIN_SERVER_URL) {
-  imports.push({
-    registration: {
-      adminServerUrl: process.env.ADMIN_SERVER_URL,
-      name: 'node',
-      serviceUrl: `http://localhost:${process.env.NODE_PORT || 8081}`,
-    },
-  });
+  imports.push(
+    ActuatorModule.forRoot({
+      registration: {
+        adminServerUrl: process.env.ADMIN_SERVER_URL,
+        name: 'node',
+        serviceUrl: `http://localhost:${process.env.NODE_PORT || 8081}`,
+      },
+    }),
+  );
 }
 
 @Module({
